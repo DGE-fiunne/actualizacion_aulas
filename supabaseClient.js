3c78b0564a33292987b549fe81708c7b8456fcb8
Mauro brite89 <mbrite89@gmail.com>
2026-09-16 14:02:35 -0300

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 // supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = '';
-const SUPABASE_ANON_KEY = '';
+const SUPABASE_URL = 'https://ulusiqeezvjhopgxsuvw.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_LOn48VrtE2AHdJxSv3J4kw_xqMFB3Rv';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
